5f3fc0f1ed752dbd26cf8bd2fec70b7e0fb49b0f
Tyler Whitsett <whitsett.tyler@gmail.com>
2015-02-26 22:01:34 -0800

    add tables to comments and styling.

--- services/LinksFactory.js
+++ services/LinksFactory.js
@@ -1,6 +1,13 @@
 hackerNews.factory('LinksFactory', function LinksFactory() {
   var factory = {};
-  factory.links = [];
+  factory.links = [
+    { name: "A Cool Story From Google or Ars Technica", rawUrl: "www.google.com", id: Math.floor((Math.random() * 100) + 1), score: 0, comments: [] },
+    { name: "A Cool Story From Google or Ars Technica", rawUrl: "www.google.com", id: Math.floor((Math.random() * 100) + 1), score: 0, comments: [] },
+    { name: "A Cool Story From Google or Ars Technica", rawUrl: "www.google.com", id: Math.floor((Math.random() * 100) + 1), score: 0, comments: [] },
+    { name: "A Cool Story From Google or Ars Technica", rawUrl: "www.google.com", id: Math.floor((Math.random() * 100) + 1), score: 0, comments: [] },
+    { name: "A Cool Story From Google or Ars Technica", rawUrl: "www.google.com", id: Math.floor((Math.random() * 100) + 1), score: 0, comments: [] },
+    { name: "A Cool Story From Google or Ars Technica", rawUrl: "www.google.com", id: Math.floor((Math.random() * 100) + 1), score: 0, comments: [] }
+  ];
   factory.addLink = function() {
     factory.links.push({ name: factory.linkName, rawUrl: factory.rawUrl, id: factory.links.length + 1, score: 0, comments: [] });
     console.log(factory.links);
