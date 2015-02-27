8ec8719473b38bea4ef23bb9802f51c4b1bcb2f6
Philip Orchard and Tyler Whitsett <whitsett.tyler@gmail.com>
2015-02-26 16:44:53 -0800

    add working comment score function.

--- controllers/LinksController.js
+++ controllers/LinksController.js
@@ -1,4 +1,16 @@
-hackerNews.controller('LinksCtrl', function LinksCtrl($scope, $stateParams, UtilitiesFactory, LinksFactory) {
+hackerNews.controller('LinksCtrl', function LinksCtrl($scope, $stateParams, LinksFactory, UtilitiesFactory) {
   $scope.links = LinksFactory.links;
   $scope.LinksFactory = LinksFactory;
+  $scope.upVote = function(id) {
+    $scope.link = UtilitiesFactory.findById(LinksFactory.links, id);
+    $scope.link.score += 1;
+  },
+  
+  $scope.links = LinksFactory.links;
+  $scope.LinksFactory = LinksFactory;
+  $scope.downVote = function(id) {
+    $scope.link = UtilitiesFactory.findById(LinksFactory.links, id);
+    $scope.link.score -= 1;
+  }
+
 });
--- services/LinksFactory.js
+++ services/LinksFactory.js
@@ -2,7 +2,7 @@ hackerNews.factory('LinksFactory', function LinksFactory() {
   var factory = {};
   factory.links = [];
   factory.addLink = function() {
-    factory.links.push({ name: factory.linkName, rawUrl: factory.rawUrl, id: factory.links.length + 1, comments: [] });
+    factory.links.push({ name: factory.linkName, rawUrl: factory.rawUrl, id: factory.links.length + 1, score: 0, comments: [] });
     console.log(factory.links);
     factory.linkName = null;
   };
