1e1e91ff51824f281537cb1c31f43f9a12804e83
Tyler Whitsett <whitsett.tyler@gmail.com>
2015-02-28 19:33:50 -0800

    add sorting for link score.

--- controllers/LinksController.js
+++ controllers/LinksController.js
@@ -1,16 +1,15 @@
 hackerNews.controller('LinksCtrl', function LinksCtrl($scope, $stateParams, LinksFactory, UtilitiesFactory) {
   $scope.links = LinksFactory.links;
   $scope.LinksFactory = LinksFactory;
+
   $scope.upVote = function(id) {
     $scope.link = UtilitiesFactory.findById(LinksFactory.links, id);
     $scope.link.score += 1;
-  },
+    console.log($scope.link.score);
+  }
   
-  $scope.links = LinksFactory.links;
-  $scope.LinksFactory = LinksFactory;
   $scope.downVote = function(id) {
     $scope.link = UtilitiesFactory.findById(LinksFactory.links, id);
     $scope.link.score -= 1;
   }
-
 });
--- services/LinksFactory.js
+++ services/LinksFactory.js
@@ -1,16 +1,15 @@
 hackerNews.factory('LinksFactory', function LinksFactory() {
   var factory = {};
   factory.links = [
-    { name: "A Cool Story From Google or Ars Technica", rawUrl: "www.google.com", id: Math.floor((Math.random() * 100) + 1), score: 0, comments: [] },
-    { name: "A Cool Story From Google or Ars Technica", rawUrl: "www.google.com", id: Math.floor((Math.random() * 100) + 1), score: 0, comments: [] },
-    { name: "A Cool Story From Google or Ars Technica", rawUrl: "www.google.com", id: Math.floor((Math.random() * 100) + 1), score: 0, comments: [] },
-    { name: "A Cool Story From Google or Ars Technica", rawUrl: "www.google.com", id: Math.floor((Math.random() * 100) + 1), score: 0, comments: [] },
-    { name: "A Cool Story From Google or Ars Technica", rawUrl: "www.google.com", id: Math.floor((Math.random() * 100) + 1), score: 0, comments: [] },
-    { name: "A Cool Story From Google or Ars Technica", rawUrl: "www.google.com", id: Math.floor((Math.random() * 100) + 1), score: 0, comments: [] }
+    { name: "A Cool Story From Google", rawUrl: "www.google.com", id: 1, score: 0, comments: [] },
+    { name: "A Cool Story From Ars Technica", rawUrl: "www.arstechnica.com", id: 2, score: 0, comments: [] },
+    { name: "A Cool Story about Becky", rawUrl: "www.google.com", id: 3, score: 0, comments: [] },
+    { name: "A Cool Story Bro", rawUrl: "www.google.com", id: 4, score: 0, comments: [] },
+    { name: "A Programming Story", rawUrl: "www.google.com", id: 5, score: 0, comments: [] },
+    { name: "A Graphics Card Review", rawUrl: "www.google.com", id: 6, score: 0, comments: [] }
   ];
   factory.addLink = function() {
     factory.links.push({ name: factory.linkName, rawUrl: factory.rawUrl, id: factory.links.length + 1, score: 0, comments: [] });
-    console.log(factory.links);
     factory.linkName = null;
   };
 
