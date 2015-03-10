532b95a38b0fdb01181c590a509d6ba0e9cb6e72
Tyler <whitsett.tyler@gmail.com>
2015-03-10 11:42:19 -0700

    add basic time based decrement for link score.

--- controllers/LinksController.js
+++ controllers/LinksController.js
@@ -2,6 +2,17 @@ hackerNews.controller('LinksCtrl', function LinksCtrl($scope, $stateParams, Link
   $scope.links = LinksFactory.links;
   $scope.LinksFactory = LinksFactory;
 
+  window.onload = function() {
+    var scoreDecay = function() {
+      for (i = 0; i < $scope.links.length; i++) {
+        $scope.links[i].score -= 1;
+      }
+      console.log($scope.links[0].score);
+      $scope.$apply();
+    }
+    window.setInterval(scoreDecay, 5000);
+  };
+
   $scope.addLink = function() {
     $scope.links.push({ name: $scope.linkName, rawUrl: $scope.rawUrl, id: $scope.links.length + 1, score: 0, comments: [] });
     $scope.linkName = null;
@@ -16,5 +27,10 @@ hackerNews.controller('LinksCtrl', function LinksCtrl($scope, $stateParams, Link
     $scope.link = UtilitiesFactory.findById(LinksFactory.links, id);
     $scope.link.score -= 1;
   };
+
+  $scope.pinStory = function(id) {
+    $scope.link = UtilitiesFactory.findById(LinksFactory.links, id);
+    $scope.link.isPinned = !scope.link.isPinned;
+  }
 
 });
